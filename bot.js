588eb277a4df4f8a767ba98e5af1c596cd6511ef
Samantha Fallahi <47825167+Samabanana@users.noreply.github.com>
2021-04-01 21:10:44 -0400

    Add files via upload
    typo

--- bot.js
+++ bot.js
@@ -5,41 +5,14 @@ const BOT_USER_ID = "827329662044733441"; //The user ID of the bot, for use some
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-// The prefix for the bot
-const PREFIX = "cn.";
-
-// necessary constants
-const ACTIVE_GAMES = [];
-
-/*
-class Game = {
-    Guild guild;
-    boolean active;
-    User redMaster, blueMaster;
-    User[] redOps, blueOps;
-    String[] wordBank;
-}
-*/
-
-// When bot is loaded, give initialization message
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 // Listen for message
 client.on('message', async msg => {
-    if (msg.author.id == BOT_USER_ID) return;   // dont reply to yourself
-    // Detect prefix
-    if (msg.content.substr(0, 3) == PREFIX) {
-        msg.content = msg.content.substr(3);    // trim message
-
-        /**
-         * PING METHOD: mostly for debug
-         * @return pong
-         */
-        if (msg.content == "ping") {
-            msg.channel.send("cn.ping");
-        }
+    if (msg.content == "ping") {
+        msg.channel.send("pong!");
     }
 });
 
